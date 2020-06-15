e8551756f2828fe9cec417117dbd7b26b4f20d9e
Rob Mixon <mixonroberth@gmail.com>
2020-06-15 13:39:39 -0500

    playing with factory objects

---
+++ scripts/doctors.js
@@ -0,0 +1,11 @@
+const createDoctor = ("name", "specialty", "addressOfPractice" => {
+    return {
+        name: name,
+        specialty: specialty,
+        addressOfPractice: 
+    }
+};
+
+const newDoctor = createDoctor("Bob" , "cancer", "1234 burpee street");
+
+console.log(newDoctor)
